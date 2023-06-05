f4c27e98e33c0c020618113695da4808c1e28cb0
tzzle <130739462+tzzle@users.noreply.github.com>
2023-06-05 18:23:49 +0300

    Add files via upload

---
+++ test/index.js
@@ -0,0 +1,41 @@
+function Btn2() {
+    document.getElementById('p-i-c-1').style.display = 'none';
+    document.getElementById('p-i-c-2').style.display = 'block';
+    document.getElementById('c1').style.backgroundColor = 'white';
+    document.getElementById('c2').style.backgroundColor = 'black';
+}
+
+function Btn3() {   
+    document.getElementById('p-i-c-2').style.display = 'none';
+    document.getElementById('p-i-c-1').style.display = 'block';
+    document.getElementById('c2').style.backgroundColor = 'white';
+    document.getElementById('c1').style.backgroundColor = 'black';
+}
+
+function Btn4() {
+    document.getElementById('p-i-c-2').style.display = 'none';
+    document.getElementById('p-i-c-3').style.display = 'block';
+    document.getElementById('c2').style.backgroundColor = 'white';
+    document.getElementById('c3').style.backgroundColor = 'black';
+}
+
+function Btn5() {
+    document.getElementById('p-i-c-3').style.display = 'none';
+    document.getElementById('p-i-c-2').style.display = 'block';
+    document.getElementById('c3').style.backgroundColor = 'white';
+    document.getElementById('c2').style.backgroundColor = 'black';
+}
+
+function Btn6() {
+    document.getElementById('p-i-c-3').style.display = 'none';
+    document.getElementById('p-i-c-4').style.display = 'block';
+    document.getElementById('c3').style.backgroundColor = 'white';
+    document.getElementById('c4').style.backgroundColor = 'black';
+}
+
+function Btn7() {
+    document.getElementById('p-i-c-4').style.display = 'none';
+    document.getElementById('p-i-c-3').style.display = 'block';
+    document.getElementById('c4').style.backgroundColor = 'white';
+    document.getElementById('c3').style.backgroundColor = 'black';
+}
